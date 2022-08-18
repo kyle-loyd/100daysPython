c5aac4203e4f7f673f3feddfc922b4838826a079
emeto <kyloyd704@gmail.com>
2022-08-17 21:52:39 -0500

    Day 30 complete!

--- Intermediate/day026-nato-alphabet/main.py
+++ Intermediate/day026-nato-alphabet/main.py
@@ -1,8 +1,19 @@
 import pandas
+
+
+def print_out():
+    name_input = input("Enter your name: ")
+    output = []
+    try:
+        for letter in name_input:
+            output.append(alphabet_dict[letter.capitalize()])
+        print(output)
+    except KeyError:
+        print("Sorry, only letters are allowed.")
+        print_out()
+
+
 alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for (_, row) in alphabet.iterrows()}
-name_input = input("Enter your name: ")
-output = []
-for letter in name_input:
-    output.append(alphabet_dict[letter.capitalize()])
-print(output)
+print_out()
+
--- Intermediate/day029-password-manager/main.py
+++ Intermediate/day029-password-manager/main.py
@@ -1,33 +1,61 @@
+import json
 from tkinter import *
 from tkinter import messagebox
-import password
+import generator
 
 
 def populate_generated_password():
     password_entry.delete(0, END)
-    password_entry.insert(0, password.generate())
+    password_entry.insert(0, generator.generate())
 
 
 def save_data():
     website = website_entry.get()
     username = username_entry.get()
     password_value = password_entry.get()
-    confirmation_msg = f"These are the details entered: \n\nEmail: {username} \nPassword: {password_value} \n\nIs it ok to save?"
     empty_fields_error = "All fields are required"
+    new_data = {
+        website: {
+            "username": username,
+            "password": password_value
+        }
+    }
 
     if website == "" or username == "" or password_value == "":
         messagebox.showerror(title="Error", message=empty_fields_error)
         return
 
-    data_entry = f"{website} | {username} | {password_value}"
-
-    is_ok = messagebox.askokcancel(title=f"Save Password: {website}", message=confirmation_msg)
-
-    if is_ok:
-        with open("data.txt", "a") as data:
-            data.write(data_entry)
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        with open("data.json", "w") as data_file:
+            json.dump(new_data, data_file, indent=4)
+    else:
+        data.update(new_data)
+        with open("data.json", "w") as data_file:
+            json.dump(data, data_file, indent=4)
+    finally:
         website_entry.delete(0, END)
         password_entry.delete(0, END)
+
+
+def search_records():
+    website = website_entry.get()
+    messagebox_title = f"Find Record: {website}"
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+            entry = data[website]
+            messagebox_message = f"Username: {entry['username']}\nPassword: {entry['password']}"
+    except FileNotFoundError:
+        messagebox.showerror(title=messagebox_title, message="No records found.")
+        return
+    except KeyError:
+        messagebox.showinfo(title=messagebox_title, message="No record found under that name.")
+    else:
+        messagebox.showinfo(title=messagebox_title, message=messagebox_message)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -47,8 +75,10 @@ canvas.grid(row=0, column=1)
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0, sticky=E, padx=LABEL_RIGHT_SPACING, pady=LABEL_VERTICAL_SPACING)
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2, sticky=EW)
+website_entry.grid(row=1, column=1, columnspan=1, sticky=EW)
 website_entry.focus()
+generate_button = Button(text="Search", command=search_records)
+generate_button.grid(row=1, column=2, sticky=EW, padx=(10, 0))
 
 username_label = Label(text="Email/Username:")
 username_label.grid(row=2, column=0, sticky=E, padx=LABEL_RIGHT_SPACING, pady=LABEL_VERTICAL_SPACING)
