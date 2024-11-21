25e7c15edead27549a1e96d45419835c300e3721
kyle-loyd <kyloyd704@gmail.com>
2024-11-21 17:58:10 -0600

    Completed day 5

---
+++ 100DaysOfPython2024/Day5/main.py
@@ -0,0 +1,26 @@
+import random
+
+
+def get_characters(count, char_list):
+    if count == 0:
+        return
+    for num in range(1, count + 1):
+        selections.append(random.choice(char_list))
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+selections = []
+get_characters(nr_letters, letters)
+get_characters(nr_numbers, numbers)
+get_characters(nr_symbols, symbols)
+
+random.shuffle(selections)
+print(str(selections))
